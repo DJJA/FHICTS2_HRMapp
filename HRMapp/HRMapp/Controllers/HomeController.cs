c2907cbd432a135c793293262335e5cdedb66620
Dirk-Jan de Beijer <djjadebeijer@gmail.com>
2017-10-13 14:45:01 +0200

    update

--- HRMapp/HRMapp/Controllers/HomeController.cs
+++ HRMapp/HRMapp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using HRMapp.ViewModels;
 using HRMapp.Logic;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using HRM.Models;
 
 namespace HRMapp.Controllers
 {
@@ -22,16 +23,38 @@ namespace HRMapp.Controllers
             return View();
         }
 
-        public IActionResult Skillset()
+        //public IActionResult Skillset()
+        //{
+        //    var skillsets = skillsetLogic.GetAll();
+
+        //    var model = new SkillsetCollectionViewModel(skillsets.ToList(), skillsets.Count()-1);
+        //    return View(model);
+        //}
+
+        public IActionResult Skillset(int id)
         {
-            var skillsets = skillsetLogic.GetAll();
-            var list = new List<SelectListItem>();
-            foreach (var skillset in skillsets)
+            var skillsets = skillsetLogic.GetAll().ToList();
+
+            int index = -1;
+            for (int i = 0; i < skillsets.Count(); i++)
             {
-                list.Add(new SelectListItem() { Text = skillset.Name, Value = skillset.Id.ToString() });
+                if (skillsets[i].Id == id) index = i;
             }
-            return View(list);
+
+            var model = new SkillsetCollectionViewModel(skillsets.ToList(), index);
+            return View(model);
         }
+
+        //public IActionResult DisplaySkillset(int id)
+        //{
+        //    Skillset skillset = null;
+        //    foreach (var skill in skillsetLogic.GetAll())
+        //    {
+        //        if (skill.Id == id)
+        //            skillset = skill;
+        //    }
+        //    return PartialView("DisplaySkillsetPView", skillset);
+        //}
 
         public IActionResult EditSkillset()
         {
---
+++ HRMapp/HRMapp/ViewModels/SkillsetCollectionViewModel.cs
@@ -0,0 +1,36 @@
+﻿using HRM.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMapp.ViewModels
+{
+    public class SkillsetCollectionViewModel
+    {
+        private List<Skillset> skillsets;
+        public List<SelectListItem> SkillsetListItems { get; private set; }
+        public List<Skillset> Skillsets
+        {
+            get { return skillsets; }
+            set
+            {
+                skillsets = value;
+                var listItems = new List<SelectListItem>();
+                foreach (var skillset in skillsets)
+                {
+                    listItems.Add(new SelectListItem() { Text = skillset.Name, Value = skillset.Id.ToString(), Selected = (Skillsets[SelectedIndex].Id == skillset.Id) });
+                }
+                SkillsetListItems = listItems;
+            }
+        }
+        public int SelectedIndex { get; private set; }
+
+        public SkillsetCollectionViewModel(List<Skillset> skillsets, int index)
+        {
+            SelectedIndex = index;
+            Skillsets = skillsets;
+        }
+    }
+}
